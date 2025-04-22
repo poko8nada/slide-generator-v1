da67db33ef785e4a77ac884fb1347f72e9599419
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-04-22 12:50:33 +0900

    implement MdDataProvider for managing markdown data

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,27 @@
+'use client'
+import { MdDataProvider } from '@/providers/md-data-provider'
+
 export default function DashboardLayout({
   children,
   markdown,
-  slide
-}: { children: React.ReactNode, markdown: React.ReactNode, slide: React.ReactNode }) {
+  slide,
+}: {
+  children: React.ReactNode
+  markdown: React.ReactNode
+  slide: React.ReactNode
+}) {
   return (
-    <div>
-      <header>
-        <h1>Dashboard Layout</h1>
-      </header>
-      <main>
-        {children}
-        {markdown}
-        {slide}
+    <MdDataProvider>
+      <div>
+        <header>
+          <h1>Dashboard Layout</h1>
+        </header>
+        <main>
+          {children}
+          {markdown}
+          {slide}
         </main>
-    </div>
+      </div>
+    </MdDataProvider>
   )
 }
---
+++ src/providers/md-data-provider.tsx
@@ -0,0 +1,28 @@
+import type React from 'react'
+import { type ReactNode, createContext, useContext, useState } from 'react'
+
+// Create the context
+const MdDataContext = createContext<
+  | { mdData: string; setMdData: React.Dispatch<React.SetStateAction<string>> }
+  | undefined
+>(undefined)
+
+// Provider component
+export const MdDataProvider = ({ children }: { children: ReactNode }) => {
+  const [mdData, setMdData] = useState<string>('')
+
+  return (
+    <MdDataContext.Provider value={{ mdData, setMdData }}>
+      {children}
+    </MdDataContext.Provider>
+  )
+}
+
+// Custom hook to use the context
+export const useMdData = () => {
+  const context = useContext(MdDataContext)
+  if (!context) {
+    throw new Error('useMdData must be used within an MdDataProvider')
+  }
+  return context
+}
