d909dc428b414777a52369d341796b43b123b962
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-04-30 18:03:22 +0900

    refactor: update Loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from 'lucide-react'
 
 export default function Loader() {
   return (
-    <div className='absolute w-full h-full flex items-center justify-center z-auto bg-neutral-200 opacity-30'>
+    <div className='absolute w-full h-full flex items-center justify-center z-auto bg-neutral-200 opacity-50'>
       <LoaderCircle className='animate-spin' size={64} />
     </div>
   )
--- src/components/mde-loader.tsx
+++
@@ -1,5 +0,0 @@
-import { Skeleton } from './ui/skeleton'
-
-export default function MdeLoader() {
-  return <Skeleton className='h-[412px] rounded-md' />
-}
