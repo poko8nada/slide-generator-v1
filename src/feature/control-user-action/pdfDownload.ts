f398e86038189e296bd577802020895e78894e50
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-05-03 17:54:43 +0900

    WIP

--- src/feature/control-user-action/pdfDownload.ts
+++ src/feature/control-user-action/pdfDownload.ts
@@ -1,4 +1,3 @@
-import { toJpeg } from 'html-to-image'
 import jsPDF from 'jspdf'
 
 export async function pdfDownload(
@@ -6,10 +5,21 @@ export async function pdfDownload(
 ) {
   if (!containerRef.current) return
 
-  const slides = document.querySelectorAll(
-    '.reveal-print .slides .pdf-page',
+  const containerClone = document
+    .querySelector('.reveal-print')
+    ?.cloneNode(true) as HTMLDivElement
+
+  if (containerClone) {
+    // 一時的に document.body に追加
+    containerClone.style.position = 'absolute'
+    containerClone.style.left = '-9999px'
+    containerClone.style.visibility = 'hidden'
+    document.body.appendChild(containerClone)
+  }
+
+  const slides = containerClone.querySelectorAll(
+    '.pdf-page',
   ) as NodeListOf<HTMLDivElement>
-  if (!slides) return
 
   const scale = 4.0
   const formatSize = [
@@ -24,26 +34,38 @@ export async function pdfDownload(
   })
 
   for (const [index, slide] of slides.entries()) {
-    try {
-      const data = await toJpeg(slide, {
-        quality: 1,
-        width: formatSize[0],
-        height: formatSize[1],
-        canvasWidth: formatSize[0],
-        canvasHeight: formatSize[1],
-        skipFonts: true,
-        style: {
-          transform: `scale(${scale})`,
-          transformOrigin: 'top left',
-        },
-      })
-
-      pdf.addImage(data, 'JPEG', 0, 0, formatSize[0], formatSize[1])
-      pdf.addPage()
-    } catch (_) {
-      console.log(index)
+    const images = slide.querySelectorAll('img')
+    for (const image of images) {
+      image.src = `${image.src}?v=${Date.now()}`
     }
+    try {
+      //   const data = await toJpeg(slide, {
+      //     quality: 1,
+      //     width: formatSize[0],
+      //     height: formatSize[1],
+      //     canvasWidth: formatSize[0],
+      //     canvasHeight: formatSize[1],
+      //     skipFonts: true,
+      //     style: {
+      //       transform: `scale(${scale})`,
+      //       transformOrigin: 'top left',
+      //     },
+      //   })
+
+      document.body.appendChild(slide)
+
+      // dataURLの詳細をコンソール出力
+      // console.log(`Slide ${index + 1} dataURL length:`, data.length)
+      // console.log(`Slide ${index + 1} dataURL prefix:`, data.substring(0, 50))
+
+      // pdf.addImage(data, 'JPEG', 0, 0, formatSize[0], formatSize[1])
+      // pdf.addPage()
+    } catch (_) {
+      console.log(index + 1, 'page failed')
+    }
+    // pdf.deletePage(pdf.getNumberOfPages())
+    // 処理後に containerClone を削除
+    // containerClone?.parentNode?.removeChild(containerClone)
+    // await pdf.save('slide.pdf', { returnPromise: true })
   }
-  pdf.deletePage(pdf.getNumberOfPages())
-  await pdf.save('slide.pdf', { returnPromise: true })
 }
