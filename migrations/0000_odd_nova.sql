a0c31a826ff9719a5d07b5737f548f01d11d6e89
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-05-29 00:48:03 +0900

    chore: remove d1/Cloudflare/Drizzle/DB関連一式

--- migrations/0000_odd_nova.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text,
-	`email` text NOT NULL,
-	`emailVerified` integer,
-	`image` text
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
